29822b01c53ec1728502097206be114b54dee9c7
Faure1 <lautarofaure@iresm.edu.ar>
2022-07-03 01:09:32 -0300

    un poco menos pesada

--- components/Scene.js
+++ components/Scene.js
@@ -1,9 +1,7 @@
 
 import React, { useRef, useEffect, Suspense } from 'react'
 import * as THREE from 'three'
-import { Color, CubeReflectionMapping } from 'three'
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 
 export const Scene = () => {
@@ -39,30 +37,10 @@ export const Scene = () => {
                 camera.updateProjectionMatrix()
         }
         window.addEventListener('resize', resize)
-        //satelite
-        const glftloader1 = new GLTFLoader()
-        glftloader1.load('./assets/untitled.gltf',
-            (gltf1) => {
-                //scene.add(gltf1.scene)
-            },
-            () => {},
-            () => {},
-        )
-        //loader
-        const glftloader = new GLTFLoader()
-        glftloader.load('./assets/amongus.gltf',
-            (gltf) => {
-                //scene.add(gltf.scene)
-            },
-            () => {},
-            () => {},
-        )
         //texturas
         const textureLoader = new THREE.TextureLoader()
         const map = textureLoader.load('./assets/Albedo.jpg')
         const aoMap = textureLoader.load('./assets/night_lights_modified.png')
-        const roughnessMap = textureLoader.load('./assets/Clouds.png')
-        const normalMap = textureLoader.load('./assets/Clouds.png' )
         const heightMap = textureLoader.load('./assets/Bump.jpg')
         //tierra
             const geometry1 = new THREE.SphereGeometry(0.8,32,16,
@@ -70,36 +48,19 @@ export const Scene = () => {
             const material2 = new THREE.MeshStandardMaterial({
                 map:map,
                 aoMap:aoMap,
-                //roughnessMap:roughnessMap,
-                //normalMap:normalMap,
                 displacementMap:heightMap,
                 displacementScale:0.05
             } )
             const circulo = new THREE.Mesh(geometry1,material2)
         scene.add(circulo)
-
-
-
-
-
-
         //lunatexturas
         const textureLoader1 = new THREE.TextureLoader()
         const map1 = textureLoader.load('./assets/lunacolor.jpg')
-        const aoMap1 = textureLoader.load('./assets/night_lights_modified.png')
-        const roughnessMap1 = textureLoader.load('./assets/Clouds.png')
-        const normalMap1 = textureLoader.load('./assets/Clouds.png' )
-        const heightMap1 = textureLoader.load('./assets/Bump.jpg')
         //luna
             const geometry = new THREE.SphereGeometry(0.3,32,16,
                 )
             const material = new THREE.MeshStandardMaterial({
                 map:map1,
-                //aoMap:aoMap1,
-                //roughnessMa1p:roughnessMap,
-                //normalMap1:normalMap,
-                //displacementMap:heightMap1,
-                //displacementScale:0.03
             } )
             const luna = new THREE.Mesh(geometry,material)
             luna.position.x = 2
@@ -115,15 +76,10 @@ export const Scene = () => {
             1
         )
         scene.add(pointLight)
-        const directionalLight =new THREE.DirectionalLight(
-            0xffffffff,
-            1.3,
-        )
         function light_update()
         {
             pointLight.position.copy( camera.position);
         }
-        //scene.add(directionalLight)
         const enviromentMap = new THREE.CubeTextureLoader()
         const envMap = enviromentMap.load([
             './assets/px.png',
@@ -133,9 +89,7 @@ export const Scene = () => {
             './assets/pz.png',
             './assets/nz.png',
         ])
-        //scene.environment = envMap
         scene.background = envMap
-        //renderer scene
 
         const animate = () =>{
             circulo.rotateY(0.004)
