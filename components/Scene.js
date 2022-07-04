1fc4dad4d2ecedaf487fc530ff33b63f6691fac3
Faure1 <lautarofaure@iresm.edu.ar>
2022-07-03 21:04:50 -0300

    3/7/2022

--- components/Scene.js
+++ components/Scene.js
@@ -1,34 +1,44 @@
 
 import React, { useRef, useEffect, Suspense } from 'react'
 import * as THREE from 'three'
+import { Plane } from 'three'
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
-
+import {gsap} from'gsap'
 
 export const Scene = () => {
     const mountRef = useRef(null)
-
+    const timeline = new gsap.timeline(
+        {defaults:{
+            duration:1,
+        }
+        }
+    )
+    
     useEffect(() => {
         const currentMount = mountRef.current
         //Scene
         const scene = new THREE.Scene()
         const camera = new THREE.PerspectiveCamera(
-            25,
-            currentMount.clientWidth / currentMount.clientHeight,
-            0.1,
-            1000
+            45, window.innerWidth / window.innerHeight,0.1,1000
         )
-        camera.position.z = 2
-        camera.position.y =3
+        camera.position.z = 45;
+        camera.position.y =4;
+        camera.position.x=3;
         scene.add (camera)
         //renderer
-        const renderer = new THREE.WebGLRenderer()
+        const renderer = new THREE.WebGLRenderer({antialias : true})
         renderer.setSize(currentMount.clientWidth,
                     currentMount.clientHeight)
         currentMount.appendChild(renderer.domElement)
         //controls
         const controls = new OrbitControls(camera, renderer.domElement)
+        controls.tartget = new THREE.Vector3(0,0,-40)
         controls.enableDamping =true
-        controls.addEventListener( 'change', light_update )
+        controls.update();
+        //suelo
+        const plane = new THREE.Mesh(new THREE.PlaneGeometry(200, 200),new THREE.MeshPhongMaterial({color:0x0a7d15 ,}))
+        plane.rotation.x = - Math.PI / 2,
+        scene.add(plane)
         //risize
         const resize = ( )=> {
             renderer.setSize(currentMount.clientWidth ,
@@ -40,46 +50,78 @@ export const Scene = () => {
         //texturas
         const textureLoader = new THREE.TextureLoader()
         const map = textureLoader.load('./assets/Albedo.jpg')
-        const aoMap = textureLoader.load('./assets/night_lights_modified.png')
-        const heightMap = textureLoader.load('./assets/Bump.jpg')
         //tierra
             const geometry1 = new THREE.SphereGeometry(0.8,32,16,
                 )
             const material2 = new THREE.MeshStandardMaterial({
                 map:map,
-                aoMap:aoMap,
-                displacementMap:heightMap,
-                displacementScale:0.05
             } )
             const circulo = new THREE.Mesh(geometry1,material2)
+            circulo.position.y=1
         scene.add(circulo)
-        //lunatexturas
-        const textureLoader1 = new THREE.TextureLoader()
-        const map1 = textureLoader.load('./assets/lunacolor.jpg')
-        //luna
-            const geometry = new THREE.SphereGeometry(0.3,32,16,
-                )
-            const material = new THREE.MeshStandardMaterial({
-                map:map1,
-            } )
-            const luna = new THREE.Mesh(geometry,material)
-            luna.position.x = 2
-        //movimiento
-        const lunaobj = new THREE.Object3D()
-        lunaobj.add(luna)
-        scene.add(lunaobj)
-        //luz
-        const A0 = new THREE.AmbientLight(0xffffffff,0.1)
-        scene.add(A0)
-        const pointLight = new THREE.PointLight(
-            0xffffffff,
-            1
-        )
-        scene.add(pointLight)
-        function light_update()
-        {
-            pointLight.position.copy( camera.position);
+        document.onkeydown = function(e){
+            if (e.key === 'ArrowLeft' ) {
+                timeline
+                    .from(circulo.position, {
+                        x:-1,
+                    })
+                    .from(circulo.rotation,
+                        {
+                            z: (Math.PI * 2)*-1,
+                        },
+                        "-=1.0"
+                        )
+                        movercam()
+            }
+            else if (e.key === 'ArrowUp' ) {
+                timeline
+                    .from(circulo.position, {
+                        z:-1,
+                    })
+                    .from(circulo.rotation,
+                        {
+                            x: Math.PI * 2,
+                        },
+                        "-=1.0"
+                    )
+                    movercam()
+            }
+            else if (e.key === 'ArrowRight' ) {
+                timeline
+                    .from(circulo.position, {
+                        x:+1,
+                    })
+                    .from(circulo.rotation,
+                        {
+                            z: (Math.PI * 2),
+                        },
+                        "-=1.0"
+                        )
+                    movercam()
+            }
+            else if (e.key === 'ArrowDown' ) {
+                timeline
+                    .from(circulo.position, {
+                        z:+1,
+                    })
+                    .from(circulo.rotation,
+                        {
+                            x: (Math.PI * 2)*-1,
+                        },
+                        "-=1.0"
+                        )
+                    movercam()
+            }
         }
+        function movercam () {
+            camera.position.x = circulo.position.x,
+            camera.position.z = (circulo.position.z)+9,
+            camera.position.y = (circulo.position.y)+2
+        }
+        //letras
+        //luz
+        const A0 = new THREE.AmbientLight(0xffffffff,0.7)
+        scene.add(A0)
         const enviromentMap = new THREE.CubeTextureLoader()
         const envMap = enviromentMap.load([
             './assets/px.png',
@@ -90,12 +132,7 @@ export const Scene = () => {
             './assets/nz.png',
         ])
         scene.background = envMap
-
         const animate = () =>{
-            circulo.rotateY(0.004)
-            luna.rotateY(0.004)
-            lunaobj.rotateY(0.02)
-            controls.update()
             renderer.render(scene,camera)
             requestAnimationFrame(animate)
         }
